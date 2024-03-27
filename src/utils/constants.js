d32679e0a2a6897a8f76047694f882b35f151686
hocak97 <hovanhoi16061997@gmail.com>
2024-03-28 00:37:52 +0700

    Update file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,8 @@
+console.log('🚀 ~ process.env:', process.env);
 
 let apiRoot = '';
 if (process.env.BUILD_MODE === 'dev') {
+
   apiRoot = 'http://localhost:8017';
 }
 if (process.env.BUILD_MODE === 'production') {
